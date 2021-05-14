d7653732cbee51ec579563b8cbbd2e2e2ec0e440
EliseevG787 <rdn787@bk.ru>
2021-05-14 20:22:00 +0300

    added DbInteraction.java

--- src/test/java/ru/netology/web/data/DbInteraction.java
+++ src/test/java/ru/netology/web/data/DbInteraction.java
@@ -103,6 +103,61 @@ public class DbInteraction {
     }
 
     @SneakyThrows
+    public static String getPaymentAmount() {
+        val statement = getStatement();
+        val rs = statement.executeQuery("SELECT * FROM payment_entity ORDER BY created DESC LIMIT 1;");
+        if (rs.next()) {
+            val amount = rs.getString("amount");
+            return amount;
+        }
+        return null;
+    }
+
+    @SneakyThrows
+    public static String getCredit_id() {
+        val statement = getStatement();
+        val rs = statement.executeQuery("SELECT * FROM order_entity ORDER BY created DESC LIMIT 1;");
+        if (rs.next()) {
+            val credit_id = rs.getString("credit_id");
+            return credit_id;
+        }
+        return null;
+    }
+
+    @SneakyThrows
+    public static String getTransaction_id() {
+        val statement = getStatement();
+        val rs = statement.executeQuery("SELECT * FROM payment_entity ORDER BY created DESC LIMIT 1;");
+        if (rs.next()) {
+            val transaction_id = rs.getString("transaction_id");
+            return transaction_id;
+        }
+        return null;
+    }
+
+    @SneakyThrows
+    public static String getPayment_id() {
+        val statement = getStatement();
+        val rs = statement.executeQuery("SELECT * FROM order_entity ORDER BY created DESC LIMIT 1;");
+        if (rs.next()) {
+            val payment_id = rs.getString("payment_id");
+            return payment_id;
+        }
+        return null;
+    }
+
+    @SneakyThrows
+    public static String getBank_id() {
+        val statement = getStatement();
+        val rs = statement.executeQuery("SELECT * FROM credit_request_entity ORDER BY created DESC LIMIT 1;");
+        if (rs.next()) {
+            val bank_id = rs.getString("bank_id");
+            return bank_id;
+        }
+        return null;
+    }
+
+    @SneakyThrows
     public static void cleanDatabase() {
         val statement = getStatement();
         statement.executeUpdate("DELETE FROM  payment_entity;");
